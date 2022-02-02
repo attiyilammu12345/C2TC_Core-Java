d9aae83af76e46d15020dbf182e935450f4376d8
attiyilammu12345 <79352489+attiyilammu12345@users.noreply.github.com>
2022-02-02 11:20:20 +0530

    first commit

---
+++ ProgramsOfC2TC/src/days/SingleInheritance.java
@@ -0,0 +1,23 @@
+package days;
+
+class Animal1 {
+	void eat() {
+		System.out.println("eating...");
+	}
+}
+
+class Dog1 extends Animal1 {
+	void bark() {
+		System.out.println("barking...");
+	}
+}
+
+public class SingleInheritance {
+
+	public static void main(String[] args) {
+		Dog1 Pug = new Dog1();
+		Pug.eat();
+		Pug.bark();
+	}
+
+}
